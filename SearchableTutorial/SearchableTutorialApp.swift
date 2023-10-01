9251c25d420d5e5fc14340cbe90245a8540ae58d
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-09-30 21:57:38 -0300

    Initial Commit

---
+++ SearchableTutorial/SearchableTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SearchableTutorialApp.swift
+//  SearchableTutorial
+//
+//  Created by Christian Manzaraz on 30/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct SearchableTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
